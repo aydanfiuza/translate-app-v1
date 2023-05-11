f43ef5e260d9b603220c2f1c4bacf8c5bd60a903
Richard Aydan Fiuza Ost <122907674+aydanfiuza@users.noreply.github.com>
2023-05-11 14:28:37 -0300

    lógica corrigida

--- login.py
+++ login.py
@@ -5,10 +5,7 @@ def ask_user():
     username = input("Informe seu nome de usuário: ")
     if username in collection_users:
         return username
-    else:
-        print("Este usuário já está cadastrado. Por favor, escolha outro.")
-        quit()
-        
+
 # Continuar checagem de senha
 # def ask_password():
 #     password = (input("Informe sua senha: "))
